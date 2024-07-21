13df6b4aade8e0bd0ee94f1971bf141113653a09
JOJO <nwin919191@gmail.com>
2024-07-21 16:36:44 +0900

    Complete optimizing

---
+++ controller/homeController.js
@@ -0,0 +1,46 @@
+import express from "express";
+import { returnMovies ,searchMovies,getMovieByCategory} from "../api/movieapi.js";
+const router = express.Router();
+
+
+router.get("/",async(req,res)=>{
+    const movieData = await returnMovies();
+    res.render("home.ejs",{"movieData":movieData});
+})
+
+router.get("/getPage",async(req,res)=>{
+    const reqPage = req.query.page;
+    const movieData = await returnMovies(reqPage);
+    res.render("home.ejs",{"movieData":movieData});
+})
+
+router.get("/search",async(req,res)=>{
+    const {movieKeywords,page} =req.query;
+    
+    const movieData = await searchMovies(movieKeywords,(page?page:1));
+    res.render("home.ejs",
+        {
+            "movieData":movieData,
+            "keyword": movieKeywords
+        });
+})
+
+
+
+router.get('/category/:name', async(req, res)=>{
+    const page = req.query.page?req.query.page:1;
+    const categoryName = req.params.name;
+    const movieData = await getMovieByCategory(categoryName,page);
+    res.render("home.ejs",
+        {
+            "movieData":movieData,
+            "keyword": categoryName
+        });
+  });
+  
+
+router.get("*",(req,res)=>{
+    res.redirect("/");
+});
+
+export default router;
--- index.js
+++ index.js
@@ -1,42 +1,4 @@
-import { returnMovies ,searchMovies,getMovieByCategory} from "./movieapi.js";
 import app from "./server.js";
+import homeRouter from "./controller/homeController.js";
 
-app.get("/",async(req,res)=>{
-    const movieData = await returnMovies();
-    res.render("home.ejs",{"movieData":movieData});
-})
-
-app.get("/getPage",async(req,res)=>{
-    const reqPage = req.query.page;
-    const movieData = await returnMovies(reqPage);
-    res.render("home.ejs",{"movieData":movieData});
-})
-
-app.get("/search",async(req,res)=>{
-    const {movieKeywords,page} =req.query;
-    
-    const movieData = await searchMovies(movieKeywords,(page?page:1));
-    res.render("home.ejs",
-        {
-            "movieData":movieData,
-            "keyword": movieKeywords
-        });
-})
-
-
-
-app.get('/category/:name', async(req, res)=>{
-    const page = req.query.page?req.query.page:1;
-    const categoryName = req.params.name;
-    const movieData = await getMovieByCategory(categoryName,page);
-    res.render("home.ejs",
-        {
-            "movieData":movieData,
-            "keyword": categoryName
-        });
-  });
-  
-
-app.get("*",(req,res)=>{
-    res.redirect("/");
-});
+app.use(homeRouter);
