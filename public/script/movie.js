84c65ce16273b1423d5510efeec920edeeb103b7
JOJO <nwin919191@gmail.com>
2024-07-27 19:40:58 +0900

    complete review page design

---
+++ public/script/movie.js
@@ -0,0 +1,7 @@
+document.querySelector(".watchlist-btn").addEventListener("click",watchlistBtn);
+
+function watchlistBtn(){
+    document.querySelector(".watchlist-add").classList.toggle("hidden");
+    document.querySelector(".watchlist-remove").classList.toggle("hidden"); 
+}
+
